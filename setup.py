c868170494c6088f546a31e41fbaa354fb890cd2
Peter Johnson <peter.d.johnson22@gmail.com>
2023-12-13 13:36:33 -0500

    Updating setup and adding requirements file

--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name="qsub",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[],
+    install_requires=install_requires,
     entry_points={
         "console_scripts": [
             "qsub_cli=qsub.main:hello",
         ],
     },
-)
-
-from setuptools import setup, find_packages
-
-setup(
-    name="qsub",
-    version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    # other configurations...
 )
