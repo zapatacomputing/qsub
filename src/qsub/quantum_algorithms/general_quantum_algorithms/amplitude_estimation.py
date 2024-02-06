288c56b2e0c550cd29504f8fdc438397268e2fbe
Peter Johnson <peter.d.johnson22@gmail.com>
2024-02-06 14:49:05 -0500

    Revise QuantumAmplitudeEstimation with iterative approach and normalization adjustments

--- src/qsub/quantum_algorithms/general_quantum_algorithms/amplitude_estimation.py
+++ src/qsub/quantum_algorithms/general_quantum_algorithms/amplitude_estimation.py
@@ -28,6 +28,7 @@ class QuantumAmplitudeEstimation(SubroutineModel):
         self,
         estimation_error: float = None,
         failure_tolerance: float = None,
+        amplitude: float = None,
     ):
         args = locals()
         # Clean up the args dictionary before setting requirements
@@ -59,8 +60,112 @@ class QuantumAmplitudeEstimation(SubroutineModel):
         subroutine_error_budget_allocation = [0.5, 0.5]
 
         # Compute number of Grover iterates needed
-        number_of_grover_iterates = compute_number_of_grover_iterates_for_amp_est(
-            consumed_failure_tolerance, self.requirements["estimation_error"]
+        number_of_grover_iterates = (
+            compute_number_of_grover_iterates_for_quantum_amp_est(
+                consumed_failure_tolerance, self.requirements["estimation_error"]
+            )
+        )
+
+        # Set number of times called to number of Grover iterates
+        self.state_preparation_oracle.number_of_times_called = (
+            2 * number_of_grover_iterates + 1
+        )
+        self.mark_subspace.number_of_times_called = 2 * number_of_grover_iterates
+
+        self.state_preparation_oracle.set_requirements(
+            failure_tolerance=subroutine_error_budget_allocation[0]
+            / self.state_preparation_oracle.number_of_times_called
+            * remaining_failure_tolerance,
+        )
+
+        self.mark_subspace.set_requirements(
+            failure_tolerance=subroutine_error_budget_allocation[1]
+            / self.mark_subspace.number_of_times_called
+            * remaining_failure_tolerance,
+        )
+
+    def count_qubits(self):
+        return self.state_preparation_oracle.count_qubits()
+
+
+def compute_number_of_grover_iterates_for_quantum_amp_est(estimation_error, amplitude):
+    # Compute number of Grover iterates needed for oblivious amplitude amplification
+    # From https://arxiv.org/abs/quant-ph/0005055
+    number_of_grover_iterates = (
+        np.pi * np.sqrt(amplitude * (1 - amplitude)) / (2 * estimation_error)
+    )
+
+    return number_of_grover_iterates
+
+
+class IterativeQuantumAmplitudeEstimation(SubroutineModel):
+    """
+    Subroutine model for the iterative quantum amplitude estimation algorithm
+    as described in https://arxiv.org/abs/1912.05559.
+    The algorithm is a variant of the quantum amplitude estimation algorithm
+    where there quantum fourier transform is not needed, each circuit is run
+    multiple times, and the number of Grover iterates used per circuit increases
+    in each iteration.
+    """
+
+    def __init__(
+        self,
+        task_name="estimate_amplitude",
+        requirements=None,
+        state_preparation_oracle: Optional[SubroutineModel] = None,
+        mark_subspace: Optional[SubroutineModel] = None,
+    ):
+        super().__init__(task_name, requirements)
+
+        if state_preparation_oracle is not None:
+            self.state_preparation_oracle = state_preparation_oracle
+        else:
+            self.state_preparation_oracle = SubroutineModel("state_preparation_oracle")
+
+        if mark_subspace is not None:
+            self.mark_subspace = mark_subspace
+        else:
+            self.mark_subspace = SubroutineModel("mark_subspace")
+
+    def set_requirements(
+        self,
+        estimation_error: float = None,
+        failure_tolerance: float = None,
+    ):
+        args = locals()
+        # Clean up the args dictionary before setting requirements
+        args.pop("self")
+        args = {
+            k: v for k, v in args.items() if v is not None and not k.startswith("__")
+        }
+        # Initialize the requirements attribute if it doesn't exist
+        if not hasattr(self, "requirements"):
+            self.requirements = {}
+
+        # Update the requirements with new values
+        self.requirements.update(args)
+
+        # Call the parent class's set_requirements method with the updated requirements
+        super().set_requirements(**self.requirements)
+
+    def populate_requirements_for_subroutines(self):
+        # Populate requirements for state_preparation_oracle and mark_subspace
+
+        # Allocate failure tolerance
+        allocation = 0.5
+        consumed_failure_tolerance = allocation * self.requirements["failure_tolerance"]
+        remaining_failure_tolerance = (
+            self.requirements["failure_tolerance"] - consumed_failure_tolerance
+        )
+
+        # Set subroutine error budget allocation
+        subroutine_error_budget_allocation = [0.5, 0.5]
+
+        # Compute number of Grover iterates needed
+        number_of_grover_iterates = (
+            compute_number_of_grover_iterates_for_iterative_amp_est(
+                consumed_failure_tolerance, self.requirements["estimation_error"]
+            )
         )
 
         # Set number of times called to number of Grover iterates
@@ -81,14 +186,13 @@ class QuantumAmplitudeEstimation(SubroutineModel):
         return self.state_preparation_oracle.count_qubits()
 
 
-def compute_number_of_grover_iterates_for_amp_est(failure_tolerance, estimation_error):
+def compute_number_of_grover_iterates_for_iterative_amp_est(
+    estimation_error, amplitude
+):
     # Compute number of Grover iterates needed for oblivious amplitude amplification
-    number_of_grover_iterates = np.log(1 / failure_tolerance) / estimation_error
+    # From https://arxiv.org/abs/quant-ph/0005055
+    number_of_grover_iterates = (
+        np.pi * np.sqrt(amplitude * (1 - amplitude)) / (2 * estimation_error)
+    )
 
     return number_of_grover_iterates
-
-
-def compute_amp_est_error_from_block_encoding(estimation_error, failure_tolerance):
-    warnings.warn("This function is not fully implemented.", UserWarning)
-    amplitude_estimation_error = 0.1
-    return amplitude_estimation_error
