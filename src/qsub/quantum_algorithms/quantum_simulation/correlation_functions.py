16bcd7aed0a7e28287c834716d8ba14c3bee1cfa
Peter Johnson <peter.d.johnson22@gmail.com>
2024-02-11 22:05:01 -0500

    Adding a subroutine for generating a unitary used in correlation function estimation

---
+++ src/qsub/quantum_algorithms/quantum_simulation/correlation_functions.py
@@ -0,0 +1,113 @@
+from typing import Optional
+from qsub.subroutine_model import SubroutineModel
+from qsub.generic_block_encoding import GenericBlockEncoding
+from qsub.utils import consume_fraction_of_error_budget
+
+
+class CorrelationFunctionUnitary(SubroutineModel):
+    def __init__(
+        self,
+        task_name="encode_correlation_function_unitary",
+        time_evolution: Optional[SubroutineModel] = None,
+        initial_state_preparation: Optional[SubroutineModel] = None,
+        controlled_block_encoding_A: Optional[GenericBlockEncoding] = None,
+        controlled_block_encoding_B: Optional[GenericBlockEncoding] = None,
+        requirements=None,
+    ):
+        super().__init__(task_name, requirements)
+
+        if time_evolution is not None:
+            self.time_evolution = time_evolution
+        else:
+            self.time_evolution = SubroutineModel("time_evolution")
+
+        if initial_state_preparation is not None:
+            self.initial_state_preparation = initial_state_preparation
+        else:
+            self.initial_state_preparation = SubroutineModel(
+                "initial_state_preparation"
+            )
+
+        if controlled_block_encoding_A is not None:
+            self.controlled_block_encoding_A = controlled_block_encoding_A
+        else:
+            self.controlled_block_encoding_A = GenericBlockEncoding(
+                "controlled_block_encoding_A"
+            )
+
+        if controlled_block_encoding_B is not None:
+            self.controlled_block_encoding_B = controlled_block_encoding_B
+        else:
+            self.controlled_block_encoding_B = GenericBlockEncoding(
+                "controlled_block_encoding_B"
+            )
+
+    def set_requirements(
+        self,
+        failure_tolerance: float = None,
+        evolution_time: float = None,
+    ):
+        args = locals()
+        # Clean up the args dictionary before setting requirements
+        args.pop("self")
+        args = {
+            k: v for k, v in args.items() if v is not None and not k.startswith("__")
+        }
+        # Initialize the requirements attribute if it doesn't exist
+        if not hasattr(self, "requirements"):
+            self.requirements = {}
+
+        # Update the requirements with new values
+        self.requirements.update(args)
+
+        # Call the parent class's set_requirements method with the updated requirements
+        super().set_requirements(**self.requirements)
+
+    def populate_requirements_for_subroutines(self):
+        remaining_failure_tolerance = self.requirements["failure_tolerance"]
+
+        # Allot error budget
+        (
+            truncation_error,
+            remaining_failure_tolerance,
+        ) = consume_fraction_of_error_budget(0.5, remaining_failure_tolerance)
+
+        (
+            time_evolution_failure_tolerance,
+            remaining_failure_tolerance,
+        ) = consume_fraction_of_error_budget(0.5, remaining_failure_tolerance)
+
+        (
+            initial_state_preparation_failure_tolerance,
+            remaining_failure_tolerance,
+        ) = consume_fraction_of_error_budget(0.5, remaining_failure_tolerance)
+
+        (
+            controlled_block_encoding_A_failure_tolerance,
+            controlled_block_encoding_B_failure_tolerance,
+        ) = consume_fraction_of_error_budget(0.5, remaining_failure_tolerance)
+
+        # Set requirements for subroutines
+
+        self.time_evolution.set_requirements(
+            failure_tolerance=time_evolution_failure_tolerance,
+            evolution_time=self.requirements["evolution_time"],
+        )
+        # Set one call for forward and one for backward evolution
+        self.time_evolution.number_of_times_called = 2
+
+        self.initial_state_preparation.set_requirements(
+            failure_tolerance=initial_state_preparation_failure_tolerance,
+        )
+        # Set one call for preparation and one for its inverse
+        self.initial_state_preparation.number_of_times_called = 2
+
+        self.controlled_block_encoding_A.set_requirements(
+            failure_tolerance=controlled_block_encoding_A_failure_tolerance,
+        )
+        self.controlled_block_encoding_A.number_of_times_called = 1
+
+        self.controlled_block_encoding_B.set_requirements(
+            failure_tolerance=controlled_block_encoding_B_failure_tolerance,
+        )
+        self.controlled_block_encoding_B.number_of_times_called = 1
