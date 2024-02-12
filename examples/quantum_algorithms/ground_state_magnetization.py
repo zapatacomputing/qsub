15f0b20490549d6de683bc25b991025dd24d062b
Peter Johnson <peter.d.johnson22@gmail.com>
2024-02-11 23:12:20 -0500

    Creating example that uses ground state preparation

---
+++ examples/quantum_algorithms/ground_state_magnetization.py
@@ -0,0 +1,21 @@
+from qsub.quantum_algorithms.general_quantum_algorithms.amplitude_estimation import (
+    QuantumAmplitudeEstimation,
+)
+from qsub.quantum_algorithms.quantum_simulation.ground_state_preparation.qsp_gsp import (
+    LinTongGroundStatePreparation,
+)
+
+
+# Initialize the subroutines
+ground_state = LinTongGroundStatePreparation()
+ground_state.set_requirements(hamiltonian_gap=0.1, initial_state_overlap=0.9)
+amplitude_estimation = QuantumAmplitudeEstimation(state_preparation_oracle=ground_state)
+
+amplitude_estimation.set_requirements(
+    estimation_error=0.001,
+    failure_tolerance=0.01,
+)
+
+# Run the profile for this subroutine
+amplitude_estimation.run_profile()
+amplitude_estimation.print_profile()
