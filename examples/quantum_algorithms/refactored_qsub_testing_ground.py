2dce36a4425b85707d93ecd7a4ab1e53fdc90e1f
Peter Johnson <peter.d.johnson22@gmail.com>
2024-03-22 11:50:59 -0400

    black reformatting

--- examples/quantum_algorithms/refactored_qsub_testing_ground.py
+++ examples/quantum_algorithms/refactored_qsub_testing_ground.py
@@ -10,28 +10,25 @@ class SubtaskRequirements(ABC):
 
 
 class Subtask(ABC):
-    """ A Subtask is simply an object that holds SubtaskRequirements.
-    """
+    """A Subtask is simply an object that holds SubtaskRequirements."""
+
     def __init__(
         self,
         requirements: SubtaskRequirements = None,
     ):
         self.requirements = requirements
 
-    def set_subtask_requirements(self,requirements:SubtaskRequirements):
+    def set_subtask_requirements(self, requirements: SubtaskRequirements):
         self.requirements = requirements
 
+
 class SubroutineModel(Subtask):
-    """ A SubroutineModel is a subtask endowed with the ability to model the servicing of 
-        that subtask by assigning requirements to its own (child) subtasks. These child subtasks 
-        are stored as attributes of the SubroutineModel. The key method of the SubroutineModel, which 
-        distinguishes it from a Subtask, is the assign_requirements_to_subtasks() method. This method
-        uses the requirements of the SubroutineModel to assign requirements to its child subtasks.
+    """A SubroutineModel is a subtask endowed with the ability to model the servicing of
+    that subtask by assigning requirements to its own (child) subtasks. These child subtasks
+    are stored as attributes of the SubroutineModel. The key method of the SubroutineModel, which
+    distinguishes it from a Subtask, is the assign_requirements_to_subtasks() method. This method
+    uses the requirements of the SubroutineModel to assign requirements to its child subtasks.
     """
-    def __init__(
-        self,
-    ):
-        super().__init__()
 
     def recursively_assign_requirements_to_all_subtasks(self):
         self.assign_requirements_to_subtasks()
@@ -41,10 +38,21 @@ class SubroutineModel(Subtask):
             if isinstance(child, SubroutineModel):
                 child.recursively_assign_requirements_to_all_subtasks()
 
-    @abstractmethod
     def assign_requirements_to_subtasks(self):
+        # Allocate failure tolerance
+        failure_budget_allocation = self.allocate_failure_tolerance_budget()
+
+        # Generate requirements for boring_task_1 and boring_task_2
+        self.generate_requirements_for_subtasks(failure_budget_allocation)
+
+    @abstractmethod
+    def allocate_failure_tolerance_budget(self):
         pass
 
+    # TODO: update name of this function
+    @abstractmethod
+    def generate_requirements_for_subtasks(self, failure_budget_allocation):
+        pass
 
 
 @dataclass
@@ -56,34 +64,59 @@ class FunTaskRequirements(SubtaskRequirements):
 class FunSubroutineModel(SubroutineModel):
     def __init__(
         self,
-        boring_task_1: Optional[Union[Subtask, SubroutineModel]] = None,
-        boring_task_2: Optional[Union[Subtask, SubroutineModel]] = None,
+        requirements: FunTaskRequirements = None,
+        boring_task_1: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
+        boring_task_2: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
     ):
-        super().__init__()
+        super().__init__(requirements=requirements)
         self.boring_task_1 = boring_task_1
         self.boring_task_2 = boring_task_2
 
-    def assign_requirements_to_subtasks(self):
-        # Allocate failure tolerance
-        failure_budget_allocation = self.allocate_failure_tolerance_budget()
-        
-        # Generate requirements for boring_task_1 and boring_task_2
-        subtask_requirements = self.generate_requirements_for_subroutines(failure_budget_allocation)
-
-        # Assign requirements to boring_task_1 and boring_task_2
-        self.set_subtask_requirements(subtask_requirements)
-        
     def allocate_failure_tolerance_budget(self):
         # Allocate failure tolerance
-        fractional_failure_budget_allocation = {"fraction_consumed": 0.5,
-                                                "fraction_to_boring_task_1": 0.25,
-                                                "fraction_to_boring_task_2": 0.25}
-        fraction_of_consumed_failure_tolerance_fraction = 0.5
+        fractional_failure_budget_allocation = {
+            "fraction_consumed": 0.5,
+            "fraction_to_boring_task_1": 0.25,
+            "fraction_to_boring_task_2": 0.25,
+        }
 
+        failure_budget_allocation = {
+            "consumed_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_consumed"
+            ]
+            * self.requirements.failure_tolerance,
+            "boring_task_1_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_to_boring_task_1"
+            ]
+            * self.requirements.failure_tolerance,
+            "boring_task_2_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_to_boring_task_2"
+            ]
+            * self.requirements.failure_tolerance,
+        }
         # Check that budget is not exceeded
-        
+        assert (
+            sum(failure_budget_allocation.values())
+            <= self.requirements.failure_tolerance
+        )
 
-        return 
+        return failure_budget_allocation
+
+    def generate_requirements_for_subtasks(self, failure_budget_allocation):
+        # Generate requirements for boring_task_1 and boring_task_2
+        boring_task_1_requirements = BoringTaskRequirements(
+            failure_tolerance=failure_budget_allocation[
+                "boring_task_1_failure_tolerance"
+            ]
+        )
+        boring_task_2_requirements = BoringTaskRequirements(
+            failure_tolerance=failure_budget_allocation[
+                "boring_task_2_failure_tolerance"
+            ]
+        )
+        self.boring_task_1.set_subtask_requirements(boring_task_1_requirements)
+        self.boring_task_2.set_subtask_requirements(boring_task_2_requirements)
+
 
 @dataclass
 class BoringTaskRequirements(SubtaskRequirements):
@@ -94,33 +127,87 @@ class BoringTaskRequirements(SubtaskRequirements):
 class BoringSubroutineModelA(SubroutineModel):
     def __init__(
         self,
-        more_boring_task: Optional[Union[Subtask, SubroutineModel]] = None,
+        more_boring_task: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
     ):
         super().__init__()
         self.more_boring_task = more_boring_task
 
-    def populate_requirements_for_subroutines(self):
+    def allocate_failure_tolerance_budget(self):
+        # Allocate failure tolerance
+        fractional_failure_budget_allocation = {
+            "fraction_consumed": 0.6,
+            "fraction_to_more_boring_task": 0.4,
+        }
 
-        # Consume failure rate
-        remaining_failure_tolerance = self.requirements.failure_tolerance / 2
-
-        self.more_boring_task.requirements = MoreBoringTaskRequirements(
-            failure_tolerance=remaining_failure_tolerance
+        failure_budget_allocation = {
+            "consumed_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_consumed"
+            ]
+            * self.requirements.failure_tolerance,
+            "more_boring_task_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_to_more_boring_task"
+            ]
+            * self.requirements.failure_tolerance,
+        }
+        # Check that budget is not exceeded
+        assert (
+            sum(failure_budget_allocation.values())
+            <= self.requirements.failure_tolerance
         )
 
-        return
+        return failure_budget_allocation
+
+    def generate_requirements_for_subtasks(self, failure_budget_allocation):
+        # Generate requirements for more_boring_task
+        more_boring_task_requirements = MoreBoringTaskRequirements(
+            failure_tolerance=failure_budget_allocation[
+                "more_boring_task_failure_tolerance"
+            ]
+        )
+        self.more_boring_task.set_subtask_requirements(more_boring_task_requirements)
 
 
 class BoringSubroutineModelB(SubroutineModel):
     def __init__(
         self,
-        more_boring_task: Optional[Union[Subtask, SubroutineModel]] = None,
+        more_boring_task: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
     ):
         super().__init__()
         self.more_boring_task = more_boring_task
 
-    def populate_requirements_for_subroutines(self):
-        return super().populate_requirements_for_subroutines()
+    def allocate_failure_tolerance_budget(self):
+        # Allocate failure tolerance
+        fractional_failure_budget_allocation = {
+            "fraction_consumed": 0.8,
+            "fraction_to_more_boring_task": 0.2,
+        }
+
+        failure_budget_allocation = {
+            "consumed_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_consumed"
+            ]
+            * self.requirements.failure_tolerance,
+            "more_boring_task_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_to_more_boring_task"
+            ]
+            * self.requirements.failure_tolerance,
+        }
+        # Check that budget is not exceeded
+        assert (
+            sum(failure_budget_allocation.values())
+            <= self.requirements.failure_tolerance
+        )
+
+        return failure_budget_allocation
+
+    def generate_requirements_for_subtasks(self, failure_budget_allocation):
+        # Generate requirements for more_boring_task
+        more_boring_task_requirements = MoreBoringTaskRequirements(
+            failure_tolerance=failure_budget_allocation[
+                "more_boring_task_failure_tolerance"
+            ]
+        )
+        self.more_boring_task.set_subtask_requirements(more_boring_task_requirements)
 
 
 @dataclass
@@ -129,10 +216,13 @@ class MoreBoringTaskRequirements(SubtaskRequirements):
     task_name: str = "more_boring_task"
 
 
-fun.subroutine = FunSubroutineModel()
-fun.boring_task_1. = BoringSubroutineModelA()
-fun.boring_task_2 = BoringSubroutineModelB()
-
 fun_requirements = FunTaskRequirements(fun_requirement=0.1, failure_tolerance=0.01)
 
-fun.run_profile()
+fun = FunSubroutineModel(requirements=fun_requirements)
+fun.boring_task_1 = BoringSubroutineModelA()
+fun.boring_task_2 = BoringSubroutineModelB()
+
+
+fun.recursively_assign_requirements_to_all_subtasks()
+
+print("fun requirements: ", asdict(fun.requirements))
