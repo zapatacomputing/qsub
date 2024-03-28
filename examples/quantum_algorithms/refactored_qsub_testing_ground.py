8bd3375471e48166873ca51e7fd30e1d63d1e7d3
Peter Johnson <peter.d.johnson22@gmail.com>
2024-03-28 12:31:16 -0600

    updating to amplitude amplification

--- examples/quantum_algorithms/refactored_qsub_testing_ground.py
+++ examples/quantum_algorithms/refactored_qsub_testing_ground.py
@@ -1,6 +1,8 @@
 from typing import Optional, Union
 from dataclasses import dataclass, asdict, is_dataclass
 from abc import ABC, abstractmethod
+import numpy as np
+from graphviz import Digraph
 
 
 @dataclass
@@ -20,6 +22,14 @@ class Subtask(ABC):
 
     def set_subtask_requirements(self, requirements: SubtaskRequirements):
         self.requirements = requirements
+
+    def allocate_aggregate_failure_tolerance_across_calls_to_self(
+        self,
+        aggregate_failure_tolerance,
+    ):
+        self.requirements.failure_tolerance = (
+            aggregate_failure_tolerance / self.requirements.number_of_times_called
+        )
 
 
 class SubroutineModel(Subtask):
@@ -42,42 +52,43 @@ class SubroutineModel(Subtask):
         # Allocate failure tolerance
         failure_budget_allocation = self.allocate_failure_tolerance_budget()
 
-        # Generate requirements for boring_task_1 and boring_task_2
-        self.generate_requirements_for_subtasks(failure_budget_allocation)
+        # Generate requirements for prepare_state and mark_subspace
+        self.assign_requirements_from_failure_budget(failure_budget_allocation)
 
     @abstractmethod
     def allocate_failure_tolerance_budget(self):
         pass
 
-    # TODO: update name of this function
     @abstractmethod
-    def generate_requirements_for_subtasks(self, failure_budget_allocation):
+    def assign_requirements_from_failure_budget(self, failure_budget_allocation):
         pass
 
 
 @dataclass
-class FunTaskRequirements(SubtaskRequirements):
-    fun_requirement: float = None
-    task_name: str = "fun_task"
+class AmplifyAmplitudeRequirements(SubtaskRequirements):
+    prepare_state_requirements: SubtaskRequirements = None
+    mark_subspace_requirements: SubtaskRequirements = None
+    initial_state_overlap: float = None
+    task_name: str = "amplify_amplitude"
 
 
-class FunSubroutineModel(SubroutineModel):
+class FixedPointAmplitudeAmplification(SubroutineModel):
     def __init__(
         self,
-        requirements: FunTaskRequirements = None,
-        boring_task_1: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
-        boring_task_2: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
+        requirements: AmplifyAmplitudeRequirements = None,
+        prepare_state: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
+        mark_subspace: Optional[Union[Subtask, SubroutineModel]] = Subtask(),
     ):
         super().__init__(requirements=requirements)
-        self.boring_task_1 = boring_task_1
-        self.boring_task_2 = boring_task_2
+        self.prepare_state = prepare_state
+        self.mark_subspace = mark_subspace
 
     def allocate_failure_tolerance_budget(self):
         # Allocate failure tolerance
         fractional_failure_budget_allocation = {
             "fraction_consumed": 0.5,
-            "fraction_to_boring_task_1": 0.25,
-            "fraction_to_boring_task_2": 0.25,
+            "fraction_to_prepare_state": 0.25,
+            "fraction_to_mark_subspace": 0.25,
         }
 
         failure_budget_allocation = {
@@ -85,12 +96,12 @@ class FunSubroutineModel(SubroutineModel):
                 "fraction_consumed"
             ]
             * self.requirements.failure_tolerance,
-            "boring_task_1_failure_tolerance": fractional_failure_budget_allocation[
-                "fraction_to_boring_task_1"
+            "prepare_state_aggregate_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_to_prepare_state"
             ]
             * self.requirements.failure_tolerance,
-            "boring_task_2_failure_tolerance": fractional_failure_budget_allocation[
-                "fraction_to_boring_task_2"
+            "mark_subspace_aggregate_failure_tolerance": fractional_failure_budget_allocation[
+                "fraction_to_mark_subspace"
             ]
             * self.requirements.failure_tolerance,
         }
@@ -102,20 +113,53 @@ class FunSubroutineModel(SubroutineModel):
 
         return failure_budget_allocation
 
-    def generate_requirements_for_subtasks(self, failure_budget_allocation):
-        # Generate requirements for boring_task_1 and boring_task_2
-        boring_task_1_requirements = BoringTaskRequirements(
-            failure_tolerance=failure_budget_allocation[
-                "boring_task_1_failure_tolerance"
-            ]
+    def assign_requirements_from_failure_budget(self, failure_budget_allocation):
+
+        # Generate number of calls to Grover iterate from consumed failure tolerance
+        number_of_grover_iterates = (
+            compute_number_of_grover_iterates_for_fixed_point_amplitude_amplification(
+                failure_budget_allocation["consumed_failure_tolerance"],
+                self.requirements.initial_state_overlap,
+            )
         )
-        boring_task_2_requirements = BoringTaskRequirements(
-            failure_tolerance=failure_budget_allocation[
-                "boring_task_2_failure_tolerance"
-            ]
+
+        # Pass input requirements to prepare_state
+        self.prepare_state.requirements = self.requirements.prepare_state_requirements
+
+        # Prepare state is called twice per Grover iterate
+        self.prepare_state.requirements.number_of_times_called = (
+            2 * number_of_grover_iterates
         )
-        self.boring_task_1.set_subtask_requirements(boring_task_1_requirements)
-        self.boring_task_2.set_subtask_requirements(boring_task_2_requirements)
+
+        # Allocate aggregate failure tolerance across calls to prepare_state
+        self.prepare_state.allocate_aggregate_failure_tolerance_across_calls_to_self(
+            failure_budget_allocation["prepare_state_aggregate_failure_tolerance"]
+        )
+
+        # Pass input requirements to mark_subspace
+        self.mark_subspace.requirements = self.requirements.mark_subspace_requirements
+
+        # Mark subspace is called once per Grover iterate
+        self.mark_subspace.requirements.number_of_times_called = (
+            number_of_grover_iterates
+        )
+
+        # Allocate aggregate failure tolerance across calls to mark_subspace
+        self.mark_subspace.allocate_aggregate_failure_tolerance_across_calls_to_self(
+            failure_budget_allocation["mark_subspace_aggregate_failure_tolerance"]
+        )
+
+
+def compute_number_of_grover_iterates_for_fixed_point_amplitude_amplification(
+    failure_tolerance, initial_state_squared_overlap
+):
+    # Compute number of Grover iterates needed for fixed point amplitude amplification
+    # from https://arxiv.org/abs/1409.3305
+    number_of_grover_iterates = np.log(2 / failure_tolerance) / np.sqrt(
+        initial_state_squared_overlap
+    )
+
+    return number_of_grover_iterates
 
 
 @dataclass
@@ -157,7 +201,7 @@ class BoringSubroutineModelA(SubroutineModel):
 
         return failure_budget_allocation
 
-    def generate_requirements_for_subtasks(self, failure_budget_allocation):
+    def assign_requirements_from_failure_budget(self, failure_budget_allocation):
         # Generate requirements for more_boring_task
         more_boring_task_requirements = MoreBoringTaskRequirements(
             failure_tolerance=failure_budget_allocation[
@@ -192,15 +236,15 @@ class BoringSubroutineModelB(SubroutineModel):
             ]
             * self.requirements.failure_tolerance,
         }
-        # Check that budget is not exceeded
+        # Check that budget is not exceeded by more than a millionth of a percent
         assert (
             sum(failure_budget_allocation.values())
-            <= self.requirements.failure_tolerance
+            <= 1.000001 * self.requirements.failure_tolerance
         )
 
         return failure_budget_allocation
 
-    def generate_requirements_for_subtasks(self, failure_budget_allocation):
+    def assign_requirements_from_failure_budget(self, failure_budget_allocation):
         # Generate requirements for more_boring_task
         more_boring_task_requirements = MoreBoringTaskRequirements(
             failure_tolerance=failure_budget_allocation[
@@ -216,13 +260,21 @@ class MoreBoringTaskRequirements(SubtaskRequirements):
     task_name: str = "more_boring_task"
 
 
-fun_requirements = FunTaskRequirements(fun_requirement=0.1, failure_tolerance=0.01)
+fun_requirements = AmplifyAmplitudeRequirements(
+    failure_tolerance=0.01,
+    initial_state_overlap=0.5,
+    prepare_state_requirements=SubtaskRequirements(),
+    mark_subspace_requirements=SubtaskRequirements(),
+)
 
-fun = FunSubroutineModel(requirements=fun_requirements)
-fun.boring_task_1 = BoringSubroutineModelA()
-fun.boring_task_2 = BoringSubroutineModelB()
+amp_amp = FixedPointAmplitudeAmplification(requirements=fun_requirements)
+amp_amp.prepare_state = BoringSubroutineModelA()
+amp_amp.mark_subspace = BoringSubroutineModelB()
 
 
-fun.recursively_assign_requirements_to_all_subtasks()
+amp_amp.recursively_assign_requirements_to_all_subtasks()
 
-print("fun requirements: ", asdict(fun.requirements))
+print("fun requirements: ", asdict(amp_amp.requirements))
+
+graph = amp_amp.display_hierarchy()
+graph.view(cleanup=True)  # This will open the generated diagram
