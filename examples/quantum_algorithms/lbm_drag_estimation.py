75e5dec6157132be4aea6fbf758131450a85a157
Peter Johnson <peter.d.johnson22@gmail.com>
2025-02-24 12:32:09 -0500

    Adding subroutine for Dalzell QLSA algorithm

--- examples/quantum_algorithms/lbm_drag_estimation.py
+++ examples/quantum_algorithms/lbm_drag_estimation.py
@@ -4,6 +4,7 @@ from qsub.subroutine_model import SubroutineModel
 
 from qsub.quantum_algorithms.general_quantum_algorithms.linear_systems import (
     TaylorQLSA,
+    DalzellQLSA,
 )
 from qsub.quantum_algorithms.general_quantum_algorithms.amplitude_amplification import (
     ObliviousAmplitudeAmplification,
@@ -31,21 +32,20 @@ from qsub.quantum_algorithms.differential_equation_solvers.linearization_methods
 import matplotlib.pyplot as plt
 from qsub.utils import calculate_max_of_solution_norm
 
+
 def generate_graphs(
     evolution_time=0.0001,
     failure_tolerance=1e-1,
     kappa_P=1,
     relative_estimation_error=0.1,
-    n_grids =  4.096e10,
-    uniform_density_deviation = 0.001,
-    fluid_nodes= 5.116*10**3
+    n_grids=4.096e10,
+    uniform_density_deviation=0.001,
+    fluid_nodes=5.116 * 10**3,
 ):
 
     mu_P_A = -0.00001
     norm_inhomogeneous_term_vector = 0.0  # ODE is homogeneous
-    norm_x_t = calculate_max_of_solution_norm(fluid_nodes,
-        uniform_density_deviation 
-    )
+    norm_x_t = calculate_max_of_solution_norm(fluid_nodes, uniform_density_deviation)
     A_stable = False
     number_of_spatial_grid_points = n_grids
     number_of_velocity_grid_points = 27
@@ -87,7 +87,6 @@ def generate_graphs(
         number_of_velocity_grid_points=number_of_velocity_grid_points,
     )
 
-
     carleman_block_encoding = CarlemanBlockEncoding(
         block_encode_linear_term=linear_term_block_encoding,
         block_encode_quadratic_term=quadratic_term_block_encoding,
@@ -100,7 +99,8 @@ def generate_graphs(
     )
 
     # Initialize Taylor QLSA
-    taylor_qlsa = TaylorQLSA()
+    # taylor_qlsa = TaylorQLSA()
+    taylor_qlsa = DalzellQLSA()
 
     # Initialize Taylor Quantum ODE Solver with choice of amplitude amplification
     taylor_ode = TaylorQuantumODESolver(
@@ -147,44 +147,55 @@ def generate_graphs(
 
     counts = drag_est.count_subroutines()
 
-    n_qubits= drag_est.count_qubits()
+    n_qubits = drag_est.count_qubits()
 
     return counts["t_gate"], n_qubits
+
 
 # failure_tolerance_values = [0.001, 0.01, 0.05]
 failure_tolerance_values = [0.01]
 grid_points = [5120.00, 5120000.00, 40960000000.00]
-fluid_nodes = [5.116*10**3,5.116*10*6, 4.093*10**10]
-evolution_times = [43.33 , 240.72 , 7703.04]
+fluid_nodes = [5.116 * 10**3, 5.116 * 10 * 6, 4.093 * 10**10]
+evolution_times = [43.33, 240.72, 7703.04]
 
 
 resources = {}
 
 for error in failure_tolerance_values:
-    resources[error]= []
+    resources[error] = []
     for g, evol_time, n_fluid_nodes in zip(grid_points, evolution_times, fluid_nodes):
-        tcounts, qubits = generate_graphs(n_grids=g, 
-            evolution_time=evol_time, 
+        tcounts, qubits = generate_graphs(
+            n_grids=g,
+            evolution_time=evol_time,
             failure_tolerance=error,
-            fluid_nodes=n_fluid_nodes
-            )
+            fluid_nodes=n_fluid_nodes,
+        )
         resources[error].append((tcounts, qubits))
 
 # Increase font sizes
-plt.rcParams.update({'font.size': 12})  # General font size
-plt.rcParams.update({'axes.titlesize': 12})  # Title font size
-plt.rcParams.update({'axes.labelsize': 12})  # Axis labels font size
-plt.rcParams.update({'xtick.labelsize': 8})  # X-tick labels font size
-plt.rcParams.update({'ytick.labelsize': 12})  # Y-tick labels font size
-plt.rcParams.update({'legend.fontsize': 12})  # Legend font size
+plt.rcParams.update({"font.size": 12})  # General font size
+plt.rcParams.update({"axes.titlesize": 12})  # Title font size
+plt.rcParams.update({"axes.labelsize": 12})  # Axis labels font size
+plt.rcParams.update({"xtick.labelsize": 8})  # X-tick labels font size
+plt.rcParams.update({"ytick.labelsize": 12})  # Y-tick labels font size
+plt.rcParams.update({"legend.fontsize": 12})  # Legend font size
 # Dark colors for different tolerances
-dark_colors = ['#000000', '#8B0000', '#00008B', '#006400', '#008B8B', '#8B008B', '#FF8C00', '#4B0082', '#2F4F4F', '#800000']
-markers = ['x', '+', 'v']
+dark_colors = [
+    "#000000",
+    "#8B0000",
+    "#00008B",
+    "#006400",
+    "#008B8B",
+    "#8B008B",
+    "#FF8C00",
+    "#4B0082",
+    "#2F4F4F",
+    "#800000",
+]
+markers = ["x", "+", "v"]
 # Tolerances and Reynolds numbers
 tolerances = list(resources.keys())
 reynolds_numbers = [1, 20, 500]
-
-
 
 
 # Plot T-gate Counts vs Failure Tolerance
@@ -193,49 +204,78 @@ ax1 = axes[0]
 for tol_idx, tolerance in enumerate(tolerances):
     for reynolds_idx, reynolds in enumerate(reynolds_numbers):
         t_counts, num_qubits = resources[tolerance][reynolds_idx]
-        ax1.scatter(tolerance, t_counts, color=dark_colors[reynolds_idx % len(dark_colors)], marker=markers[reynolds_idx % len(markers)], label=f'Re {reynolds}' if tol_idx == 0 else "")
-ax1.set_yscale('log')
-ax1.set_ylabel('$T$ gate Counts')
-ax1.set_title('$T$ gate Counts vs Failure Tolerance')
+        ax1.scatter(
+            tolerance,
+            t_counts,
+            color=dark_colors[reynolds_idx % len(dark_colors)],
+            marker=markers[reynolds_idx % len(markers)],
+            label=f"Re {reynolds}" if tol_idx == 0 else "",
+        )
+ax1.set_yscale("log")
+ax1.set_ylabel("$T$ gate Counts")
+ax1.set_title("$T$ gate Counts vs Failure Tolerance")
 ax1.grid(True)
-ax1.legend(title='Legend')
+ax1.legend(title="Legend")
 
 # Plot Number of Qubits vs Failure Tolerance
 ax2 = axes[1]
 for tol_idx, tolerance in enumerate(tolerances):
     for reynolds_idx, reynolds in enumerate(reynolds_numbers):
         t_counts, num_qubits = resources[tolerance][reynolds_idx]
-        ax2.scatter(tolerance, num_qubits, color=dark_colors[reynolds_idx % len(dark_colors)], marker=markers[reynolds_idx % len(markers)], label=f'Re {reynolds}' if tol_idx == 0 else "")
-ax2.set_yscale('log')
-ax2.set_xlabel('Failure Tolerance')
-ax2.set_ylabel('Number of Qubits')
-ax2.set_title('Number of Qubits vs Failure Tolerance')
+        ax2.scatter(
+            tolerance,
+            num_qubits,
+            color=dark_colors[reynolds_idx % len(dark_colors)],
+            marker=markers[reynolds_idx % len(markers)],
+            label=f"Re {reynolds}" if tol_idx == 0 else "",
+        )
+ax2.set_yscale("log")
+ax2.set_xlabel("Failure Tolerance")
+ax2.set_ylabel("Number of Qubits")
+ax2.set_title("Number of Qubits vs Failure Tolerance")
 ax2.grid(True)
-ax2.legend(title='Legend')
+ax2.legend(title="Legend")
 plt.tight_layout()
 plt.show()
 
 
 # Plot
 fig, axes2 = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
-fig.suptitle('$T$ gate counts  vs. Number of Qubits for different Reynolds numbers')
+fig.suptitle("$T$ gate counts  vs. Number of Qubits for different Reynolds numbers")
 
 # Dark colors for different tolerances
-dark_colors = ['#000000', '#00008B', '#006400', '#8B0000', '#008B8B', '#8B008B', '#FF8C00', '#4B0082', '#2F4F4F', '#800000']
-markers = ['x', 'o', '^', 's', 'p', '*', 'D', 'v', '<', '>']
+dark_colors = [
+    "#000000",
+    "#00008B",
+    "#006400",
+    "#8B0000",
+    "#008B8B",
+    "#8B008B",
+    "#FF8C00",
+    "#4B0082",
+    "#2F4F4F",
+    "#800000",
+]
+markers = ["x", "o", "^", "s", "p", "*", "D", "v", "<", ">"]
 
 for i, reynolds in enumerate(reynolds_numbers):
     ax = axes2[i]
     for tol_idx, tolerance in enumerate(tolerances):
         t_counts, num_qubits = resources[tolerance][i]
-        ax.scatter(num_qubits, t_counts, color=dark_colors[tol_idx], marker=markers[tol_idx], label=f'Tolerance={tolerance}')
-    ax.set_title(f'Reynolds={reynolds}')
-    ax.set_xlabel('Number of Qubits')
-    ax.set_yscale('log')
+        ax.scatter(
+            num_qubits,
+            t_counts,
+            color=dark_colors[tol_idx],
+            marker=markers[tol_idx],
+            label=f"Tolerance={tolerance}",
+        )
+    ax.set_title(f"Reynolds={reynolds}")
+    ax.set_xlabel("Number of Qubits")
+    ax.set_yscale("log")
     ax.legend(title="Failure Tolerance")
     ax.grid(True)
 
-axes2[0].set_ylabel('$T$ gate counts')
+axes2[0].set_ylabel("$T$ gate counts")
 # fig.legend(loc='center right', bbox_to_anchor=(1.1, 0.5))
 plt.tight_layout(rect=[0, 0, 1, 0.96])
 plt.show()
@@ -246,16 +286,21 @@ for i, reynolds in enumerate(reynolds_numbers):
     ax = axes3[i]
     for tol_idx, tolerance in enumerate(tolerances):
         t_counts, num_qubits = resources[tolerance][i]
-        product_t_counts_num_qubits = t_counts*num_qubits
-        ax.scatter(num_qubits, product_t_counts_num_qubits, color=dark_colors[tol_idx], marker=markers[tol_idx], label=f'Tolerance={tolerance}')
-    ax.set_title(f'Reynolds={reynolds}')
-    ax.set_xlabel('Number of Qubits')
-    ax.set_yscale('log')
+        product_t_counts_num_qubits = t_counts * num_qubits
+        ax.scatter(
+            num_qubits,
+            product_t_counts_num_qubits,
+            color=dark_colors[tol_idx],
+            marker=markers[tol_idx],
+            label=f"Tolerance={tolerance}",
+        )
+    ax.set_title(f"Reynolds={reynolds}")
+    ax.set_xlabel("Number of Qubits")
+    ax.set_yscale("log")
     ax.legend(title="Failure Tolerance")
     ax.grid(True)
 
-axes3[0].set_ylabel('$T$ counts $\\times$ Number of Logical Qubits')
+axes3[0].set_ylabel("$T$ counts $\\times$ Number of Logical Qubits")
 # fig.legend(loc='center right', bbox_to_anchor=(1.1, 0.5))
-plt.tight_layout(rect=[0, 0, 1, 0.96])# 
+plt.tight_layout(rect=[0, 0, 1, 0.96])  #
 plt.show()
-
--- src/qsub/quantum_algorithms/general_quantum_algorithms/linear_systems.py
+++ src/qsub/quantum_algorithms/general_quantum_algorithms/linear_systems.py
@@ -164,3 +164,128 @@ def get_taylor_qlsa_num_block_encoding_calls(
     #     number_of_calls_to_b = float(number_of_calls_to_b)
 
     return number_of_calls_to_A, number_of_calls_to_b
+
+
+class DalzellQLSA(SubroutineModel):
+    def __init__(
+        self,
+        task_name="solve_quantum_linear_system",
+        requirements=None,
+        linear_system_block_encoding: Optional[GenericLinearSystemBlockEncoding] = None,
+        prepare_b_vector: Optional[SubroutineModel] = None,
+    ):
+        super().__init__(task_name, requirements)
+
+        if linear_system_block_encoding is not None:
+            self.linear_system_block_encoding = linear_system_block_encoding
+        else:
+            self.linear_system_block_encoding = GenericLinearSystemBlockEncoding(
+                "linear_system_block_encoding"
+            )
+
+        if prepare_b_vector is not None:
+            self.prepare_b_vector = prepare_b_vector
+        else:
+            self.prepare_b_vector = SubroutineModel("prepare_b_vector")
+
+    def set_requirements(
+        self,
+        failure_tolerance: float = None,
+    ):
+        args = locals()
+        # Clean up the args dictionary before setting requirements
+        args.pop("self")
+        args = {
+            k: v for k, v in args.items() if v is not None and not k.startswith("__")
+        }
+        # Initialize the requirements attribute if it doesn't exist
+        if not hasattr(self, "requirements"):
+            self.requirements = {}
+
+        # Update the requirements with new values
+        self.requirements.update(args)
+
+        # Call the parent class's set_requirements method with the updated requirements
+        super().set_requirements(**self.requirements)
+
+    def populate_requirements_for_subroutines(self):
+        # Allocate failure tolerance
+        remaining_failure_tolerance = self.requirements["failure_tolerance"]
+        (
+            solve_linear_system_failure_tolerance,
+            remaining_failure_tolerance,
+        ) = consume_fraction_of_error_budget(0.5, remaining_failure_tolerance)
+
+        (
+            linear_system_block_encoding_failure_tolerance,
+            remaining_failure_tolerance,
+        ) = consume_fraction_of_error_budget(0.5, remaining_failure_tolerance)
+
+        prepare_b_vector_failure_tolerance = remaining_failure_tolerance
+
+        subnormalization = self.linear_system_block_encoding.get_subnormalization()
+        condition_number = self.linear_system_block_encoding.get_condition_number()
+
+        # Compute number of calls to block encoding of linear system and b vector prep
+        (n_calls_to_A, n_calls_to_b) = get_dalzell_qlsa_num_block_encoding_calls(
+            solve_linear_system_failure_tolerance,
+            subnormalization,
+            condition_number,
+        )
+        self.linear_system_block_encoding.number_of_times_called = n_calls_to_A
+        self.prepare_b_vector.number_of_times_called = n_calls_to_b
+
+        # Set block encoding requirements
+        self.linear_system_block_encoding.set_requirements(
+            failure_tolerance=linear_system_block_encoding_failure_tolerance
+            / n_calls_to_A,
+        )
+
+        # Set block encoding requirements
+        self.prepare_b_vector.set_requirements(
+            failure_tolerance=prepare_b_vector_failure_tolerance / n_calls_to_b,
+        )
+
+    def count_qubits(self):
+        # TODO: Implement the qubit count for the Dalzell QLSA algorithm
+        return self.linear_system_block_encoding.count_qubits() + 6
+
+
+def get_dalzell_qlsa_num_block_encoding_calls(
+    failure_probability: float,
+    subnormalization: Union[float, Basic],
+    condition_number: Union[float, Basic],
+) -> tuple[Union[float, Basic], Union[float, Basic]]:
+    """Get the number of block encoding calls for the Taylor QLSA algorithm
+    of arXiv:2305.11352 as shown in Thm. 1.
+
+    Args:
+        failure_probability: The tolerable probability of the algorithm failing due to
+            approximation error in the solution state.
+        subnormalization: The subnormalization value (can be a sympy object).
+        condition_number: The condition number (can be a sympy object).
+
+    Returns:
+        number_of_calls_to_A: The number of block encoding calls to A (can be a float or sympy object).
+        number_of_calls_to_b: The number of queries to state preparation of b (can be a float or sympy object).
+    """
+    # TODO: update or remove these conditions
+    if not isinstance(condition_number, Basic) and (
+        failure_probability > 0.24 or condition_number < sqrt(12)
+    ):
+        raise ValueError(
+            "failure_probability must be less than or equal to 0.24 and condition_number must be greater than or equal to sqrt(12)."
+        )
+
+    number_of_calls_to_A = result = (
+        56.0 * condition_number
+        + 1.05
+        * condition_number
+        * math.log(math.sqrt(1 - failure_probability**2) / failure_probability)
+        + 2.78 * math.log(condition_number) ** 3
+        + 3.17
+    )
+
+    number_of_calls_to_b = 2 * number_of_calls_to_A
+
+    return number_of_calls_to_A, number_of_calls_to_b
